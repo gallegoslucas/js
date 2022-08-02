de26297e766f03174f8ba90018b77b81d3d8690a
lucasgallegos <lucas.gallegos@hotmail.com>
2022-08-02 16:37:08 +0200

    js4

--- codigo.js
+++ codigo.js
@@ -1,6 +1,4 @@
 
-let pedido = prompt("Que pizza quiere pedir? (esc para salir)");
-
 let cantMargarita = 0;
 let cantFugazzeta = 0;
 let cantRucula = 0;
@@ -8,40 +6,50 @@ let total = 0;
 let rucula = 800;
 let fugazzeta = 700;
 let margarita = 600;
-do {
-    switch (pedido) {
-        case "margarita" :
-            cantMargarita++;
-            total = total + margarita;
-            alert("Se agrego una margarita a tu pedido")
-            break;
-        case "rucula": 
-            cantRucula++;
-            total = total + rucula;
-            alert("Se agrego una rucula a tu pedido")
-            break;
-        case "fugazzeta":
-            cantFugazzeta++;
-            total = total + fugazzeta;
-            alert("Se agrego una fugazzeta a tu pedido")
-            break;
-        default:
-            alert("no contamos con esa variedad");
-            break;
+
+    let pedido = prompt("Que pizza quiere pedir? (esc para terminar)");
+    do {
+        switch (pedido) {
+            case "margarita" :
+                cantMargarita++;
+                total = total + margarita;
+                alert("Se agrego una margarita a tu pedido")
+                break;
+            case "rucula": 
+                cantRucula++;
+                total = total + rucula;
+                alert("Se agrego una rucula a tu pedido")
+                break;
+            case "fugazzeta":
+                cantFugazzeta++;
+                total = total + fugazzeta;
+                alert("Se agrego una fugazzeta a tu pedido")
+                break;
+            default:
+                alert("no contamos con esa variedad");
+                break;
+        }
+        pedido = prompt("Que pizza quiere pedir? (esc para salir)");
+        
+    } while (pedido != "esc")
+   
+
+    console.log("Su pedido es : \n" + cantFugazzeta + " Fugazzeta \n" + cantMargarita + " Margarita \n" + cantRucula + " Rucula")
+
+    console.log("Total del pedido : $" + total)
+    
+let pregDesc = prompt("Ingrese cupon de descuento")
+    if (pregDesc=="descuento"){
+    function calcularDesc(total) {
+        return total * 0.9;
     }
-    pedido = prompt("Que pizza quiere pedir? (esc para salir)");
-    
-    
+    let totalDesc = calcularDesc(total);
+        console.log("Total con descuento : $" + totalDesc)
+    }else{alert("Este cupon expiro o no es valido")}
 
-} while (pedido != "esc")
-
-
+ 
 
 
-    
-console.log("Su pedido es : \n" + cantFugazzeta + " Fugazzeta \n" + cantMargarita +" Margarita \n"+ cantRucula+ " Rucula")
-
-console.log("Total del pedido : $"+ total)
 
 
 
